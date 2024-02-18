4563b107267f24102e4919a9be7fa7e64444b3a6
BLACKHAT <156422271+BLACKHATSGODBOLTE@users.noreply.github.com>
2024-02-18 03:06:59 -0500

    Add files via upload

---
+++ enumeration.ps1
@@ -0,0 +1,8 @@
+$PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
+$DN = ([adsi]'').distinguishedName 
+$LDAP = "LDAP://$PDC/$DN"
+
+$direntry = New-Object System.DirectoryServices.DirectoryEntry($LDAP)
+
+$dirsearcher = New-Object System.DirectoryServices.DirectorySearcher($direntry)
+$dirsearcher.FindAll()
